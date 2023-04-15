cc222d680aa8924ddb2357e8efe75381789b2c51
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-15 14:23:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const sizeInfo = document.getElementById('sizeInfo');
+const h1 = sizeInfo.querySelector('h1');
+
+// Get initial window size
+h1.innerText = `Width: ${window.innerWidth}, Height: ${window.innerHeight}`;
+
+// Update size on window resize
+window.addEventListener('resize', () => {
+	h1.innerText = `Width: ${window.innerWidth}, Height: ${window.innerHeight}`;
+});
